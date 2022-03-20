a30e8816a196dbb23d208a11affb2d2274cd4966
Fabricio Garcia <fabgrel10@gmail.com>
2022-03-19 21:23:26 -0600

    💄 style: Add style to Nabvar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
+import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+
+import { ReactComponent as CrwnLogo } from '../../assets/images/crown.svg';
+
+import './Navbar.scss';
 
 const Navbar = () => (
   <>
     <div className="navbar">
       <Link className="navbar__logo-container" to="/">
-        <div>Logo</div>
+        <CrwnLogo className="navbar__logo" />
       </Link>
       <div className="navbar__links">
         <Link className="navbar__link" to="/">
