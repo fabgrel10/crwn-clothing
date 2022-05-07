abf7cb1e26d1f86d685dea9251e80bc7f1bf75fc
Fabricio Garcia <fabgrel10@gmail.com>
2022-05-07 15:45:59 -0500

    ⬆️ Upgrade project dependencies (react v18)

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-
 import './main.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
